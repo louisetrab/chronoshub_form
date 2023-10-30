341490be5db4e9837c4693aae0ac72b40d51c979
louisetrab <louisetrab@gmail.com>
2023-10-30 14:12:40 +0100

    Arrow functions

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener("DOMContentLoaded", () => {
   const apiKey = "54a1a4ac02b3704da065071ad9f54743";
   const apiUrl = "https://restcountries.com/v3.1/";
 
-  // Function for country dropdown
-  function populateCountryDropdown() {
+  // Arrow function for country dropdown
+  const populateCountryDropdown = () => {
     const countrySelect = document.getElementById("country-select");
     fetch(apiUrl + "all?apiKey=" + apiKey)
       .then((response) => response.json())
@@ -11,17 +11,17 @@ document.addEventListener("DOMContentLoaded", function () {
         // Countries in alphabetical order
         data.sort((a, b) => a.name.common.localeCompare(b.name.common));
 
-        data.forEach(function (country) {
+        data.forEach((country) => {
           const option = document.createElement("option");
           option.value = country.name.common;
           option.text = country.name.common;
           countrySelect.appendChild(option);
         });
       });
-  }
+  };
 
-  // Function to get country information based on the selection
-  function getCountryInfo(selectedCountryName) {
+  // Arrow function to get country information based on the selection
+  const getCountryInfo = (selectedCountryName) => {
     fetch(apiUrl + "name/" + selectedCountryName + "?apiKey=" + apiKey)
       .then((response) => response.json())
       .then((countryData) => {
@@ -31,20 +31,17 @@ document.addEventListener("DOMContentLoaded", function () {
         document.getElementById("population").textContent =
           selectedCountry.population.toLocaleString();
       });
-  }
+  };
 
   // Fill the country dropdown as soon as the page loads
   populateCountryDropdown();
 
   // Submit button
-  document
-    .getElementById("country-form")
-    .addEventListener("submit", function (e) {
-      e.preventDefault();
-      const selectedCountryName =
-        document.getElementById("country-select").value;
-      if (selectedCountryName) {
-        getCountryInfo(selectedCountryName);
-      }
-    });
+  document.getElementById("country-form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    const selectedCountryName = document.getElementById("country-select").value;
+    if (selectedCountryName) {
+      getCountryInfo(selectedCountryName);
+    }
+  });
 });
